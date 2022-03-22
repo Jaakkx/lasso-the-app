19752a008b7672da5cb3d71e45152b4f6c9b05ff
Antobdr <antonin.borderie@orange.fr>
2022-03-22 12:45:15 +0100

    modif front v2

--- front/src/scenes/AllAsso/AllAsso.tsx
+++ front/src/scenes/AllAsso/AllAsso.tsx
@@ -1,12 +1,36 @@
 import React, { useEffect } from "react";
 import "./AllAsso.css";
+import AssoCard from "../../components/AssoCard/AssoCard";
 
 const AllAsso = () => {
   useEffect(() => {
     document.getElementsByClassName("App")[0].id = "appAllAsso";
   });
 
-  return <div id="all-asso">Les associations</div>;
+  return(
+    <div className="main">
+        <div className="flux_ranked_card">
+        <h1 className="title_ranked_page">Mes Asso'</h1>
+    <AssoCard 
+        TitleCard="Nom de l'Asso que je souhaite rejoindre" 
+        ContentCard="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu lobortis tortor sem lacus, dictumst consectetur urna. Consequat, eleifend in id egestas at ultricies vitae sit.Lorem ipsum dolor sit amet, consectetur.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu lobortis tortor sem lacus, dictumst consectetur urna. Consequat, eleifend in id egestas at ultricies vitae sit.Lorem ipsum dolor sit amet, consectetur.
+        "
+        LogoCard="logo.jpg"
+        
+    />
+    <AssoCard 
+    TitleCard="Nom de l'Asso2"
+    ContentCard="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu lobortis tortor sem lacus, dictumst consectetur urna. Consequat, eleifend in id egestas at ultricies vitae sit."
+    
+    />
+    <AssoCard 
+    TitleCard="Nom de l'Asso3"
+    ContentCard="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Arcu lobortis tortor sem lacus, dictumst consectetur urna. Consequat, eleifend in id egestas at ultricies vitae sit."
+    
+    />
+    </div>
+    </div>
+);
 };
 
 export default AllAsso;
